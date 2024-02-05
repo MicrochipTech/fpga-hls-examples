4cd18c399619f6087ff04bf4b77630a8b72d948f
Jennifer Mah <jennifer.mah@microchip.com>
2024-02-05 16:45:45 -0500

    Fix typo

--- Training4/pdf2md.sh
+++ Training4/pdf2md.sh
@@ -1,4 +1,4 @@
-# To convert from PDF -> md
+# To convert from docx -> md
 pandoc \
     -f docx \
     -t markdown \
@@ -9,4 +9,10 @@ pandoc \
     -o readme.md
 
 # To convert from md -> PDF
+# We tried using pandoc in the following way:
 #pandoc readme.md -o output.pdf --table-of-contents
+# Which didn't work because of the latex formula.
+
+# THIS IS WHAT WE USED TO GENERATE THE PDF
+# VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=yzane.markdown-pdf
+
