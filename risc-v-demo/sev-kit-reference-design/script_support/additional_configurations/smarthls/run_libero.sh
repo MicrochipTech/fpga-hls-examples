83325b050a3a435d53a25e99e0ecfb810a3a45d5
Jennifer Mah <jennifer.mah@microchip.com>
2024-08-09 12:09:57 -0400

    run_libero.sh added workaround for synplify M-2

--- risc-v-demo/sev-kit-reference-design/script_support/additional_configurations/smarthls/run_libero.sh
+++ risc-v-demo/sev-kit-reference-design/script_support/additional_configurations/smarthls/run_libero.sh
@@ -32,8 +32,18 @@ time libero \
 
 echo "------[ Identify ]--------"
 pushd ./SEVPFSOC_H264/synthesis/
-synplify_pro -licensetype synplifypro_actel -batch ../../script_support/additional_configurations/smarthls/synplify_enable_dbg_mode.tcl
-identify_instrumentor_shell -licensetype identinstrumentor_actel ../../script_support/additional_configurations/smarthls/identify_instrument.tcl
+
+if [[ -z "${HLS_SYNPLIFY_PATH}" ]]; then
+        SYNPLIFY_EXE="synplify_pro"
+        IDENTIFY_EXE="identify_instrumentor_shell"
+else
+        SYNPLIFY_EXE=$HLS_SYNPLIFY_PATH
+        IDENTIFY_EXE=$(dirname $HLS_SYNPLIFY_PATH)"/identify_instrumentor_shell"
+fi
+
+
+$SYNPLIFY_EXE -licensetype synplifypro_actel -batch ../../script_support/additional_configurations/smarthls/synplify_enable_dbg_mode.tcl
+$IDENTIFY_EXE -licensetype identinstrumentor_actel ../../script_support/additional_configurations/smarthls/identify_instrument.tcl
 popd
 
 echo "------[ Full build ]--------"
