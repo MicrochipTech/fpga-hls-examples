8b4ce15089f8f57392a435d79722e4b416d03da4
Shuran Xu <shuran.xu@microchip.com>
2025-04-23 14:13:47 -0400

    Removed risc-v-demo/precompiled and SinCosTable_Generator_20210112_1730.zip from Training 3 to resolve the Github bandwidth issue for SmartHLS2025.1 release.

--- risc-v-demo/precompiled/shls_sw_dependencies/download_precompiled_libraries.ps1
+++
@@ -1,7 +0,0 @@
-# Note: the precompiled libraries are included with your installation of SmartHLS
-#       under SMARTHLS_INSTALL_DIR\smarthls-library\external\vision\precompiled_sw_libraries
-
-# Determine the location of SmartHLS
-$SHLS_ROOT_DIR = Split-Path -Parent (Split-Path -Parent (Get-Command shls).Source)
-
-Copy-Item "$SHLS_ROOT_DIR\smarthls-library\external\vision\precompiled_sw_libraries\*" -Destination ".\" -Recurse -Exclude "*.tar.gz"
--- risc-v-demo/precompiled/shls_sw_dependencies/download_precompiled_libraries.sh
+++
@@ -1,4 +0,0 @@
-SHLS_ROOT_DIR=$(dirname "$(dirname "$(which shls)")")
-echo $SHLS_ROOT_DIR
-
-rsync -av --exclude='*.tar.gz' $SHLS_ROOT_DIR/smarthls-library/external/vision/precompiled_sw_libraries/* ./
